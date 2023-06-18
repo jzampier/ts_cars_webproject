a66f6a0a2625ced250b8f97aabdd518737bffd73
Julio Zampier <juliozampier@gmail.com>
2023-06-18 12:07:01 -0300

    59:52 combobox setup in progress

---
+++ components/SearchBar.tsx
@@ -0,0 +1,20 @@
+'use client';
+import { useState } from 'react';
+import { SearchManufacturer } from './';
+
+const SearchBar = () => {
+  const [manufacturer, setManufacturer] = useState('');
+  const handleSearch = () => {};
+  return (
+    <form onSubmit={handleSearch} className="searchbar">
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+      </div>
+    </form>
+  );
+};
+
+export default SearchBar;
