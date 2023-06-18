361dab27c6d58e7b6cee8d2bebca12480daf689a
Julio Zampier <juliozampier@gmail.com>
2023-06-18 17:43:35 -0300

    1:24:02 fetching api

--- utils/index.ts
+++ utils/index.ts
@@ -1,16 +1,15 @@
-const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
-const options = {
-  method: 'GET',
-  headers: {
+export async function fetchCars() {
+  const headers = {
     'X-RapidAPI-Key': '8339d99db8msh5c9b0085014cd10p146f69jsna853f8017701',
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
-  },
-};
+  };
 
-try {
-  const response = await fetch(url, options);
-  const result = await response.text();
-  console.log(result);
-} catch (error) {
-  console.error(error);
+  const response = await fetch(
+    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',
+    { headers: headers }
+  );
+
+  const result = await response.json();
+
+  return result;
 }
