6abddc84a90e198fad28abcb71ca645bdd537ec8
Julio Zampier <juliozampier@gmail.com>
2023-06-17 22:05:16 -0300

    footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { footerLinks } from '@/constants';
+
 const Footer = () => {
   return (
     <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
@@ -17,7 +19,22 @@ const Footer = () => {
             Todos os direitos reservados &copy;
           </p>
         </div>
-        <div className="footer__links"></div>
+        <div className="footer__links">
+          {footerLinks.map((link) => (
+            <div className="footer__link" key={link.title}>
+              <h3 className="font-bold">{link.title}</h3>
+              {link.links.map((item) => (
+                <Link
+                  key={item.title}
+                  href={item.url}
+                  className="text-gray-500"
+                >
+                  {item.title}
+                </Link>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
     </footer>
   );
