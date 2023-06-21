2207804b040cac1d4a4d393f896e6d3ac9002559
Julio Zampier <juliozampier@gmail.com>
2023-06-21 19:54:24 -0300

    Card modal ongoing

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import { useState } from 'react';
 import CustomButton from './CustomButton';
 import { calculateCarRent } from '@/utils';
+import CarDetails from './CarDetails';
 
 interface CarCardProps {
   car: CarProps;
@@ -72,6 +73,11 @@ const CarCard = ({ car }: CarCardProps) => {
           />
         </div>
       </div>
+      <CarDetails
+        isOpen={isOpen}
+        closeModal={() => setIsOpen(false)}
+        car={car}
+      />
     </div>
   );
 };
